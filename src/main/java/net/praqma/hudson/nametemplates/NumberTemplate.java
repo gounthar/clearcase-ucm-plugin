3fe4ed42a0c80a3e5debd3210aaab1f3850bb8c5
Bruno Verachten <gounthar@gmail.com>
2025-03-29 22:55:33 +0100

    fix: Improve null safety in NumberTemplate parse method

--- src/main/java/net/praqma/hudson/nametemplates/NumberTemplate.java
+++ src/main/java/net/praqma/hudson/nametemplates/NumberTemplate.java
@@ -5,13 +5,12 @@ import net.praqma.hudson.CCUCMBuildAction;
 
 public class NumberTemplate extends Template {
 
-	@Override
-	public String parse( CCUCMBuildAction action, String args, FilePath ws ) {
-		try {
-			return action.getBuild().getNumber()+"";
-		} catch( NullPointerException e ) {
-			return "?";
-		}
-	}
+    @Override
+    public String parse(CCUCMBuildAction action, String args, FilePath ws) {
+        if (action == null || action.getBuild() == null) {
+            return "?";
+        }
+        return action.getBuild().getNumber() + "";
+    }
 
 }
